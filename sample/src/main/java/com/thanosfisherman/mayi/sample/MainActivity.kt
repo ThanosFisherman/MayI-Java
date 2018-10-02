b7377d35750957a1310683eea0328fef511c9284
Thanos Psaridis <psaridis@gmail.com>
2018-10-02 14:56:29 +0300

    Refactored MainActivity

--- sample/src/main/java/com/thanosfisherman/mayi/sample/MainActivity.kt
+++ sample/src/main/java/com/thanosfisherman/mayi/sample/MainActivity.kt
@@ -6,7 +6,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.thanosfisherman.mayi.Mayi
+import com.thanosfisherman.mayi.MayI
 import com.thanosfisherman.mayi.PermissionBean
 import com.thanosfisherman.mayi.PermissionToken
 import java.util.*
@@ -18,30 +18,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val buttonContacts = findViewById<Button>(R.id.contacts_permission_button)
-        buttonContacts.setOnClickListener { _ ->
-            Mayi.withActivity(this)
+        buttonContacts.setOnClickListener {
+            MayI.withActivity(this)
                     .withPermission(Manifest.permission.READ_CONTACTS)
-                    .onResult { this.permissionResultSingle(it) }
-                    .onRationale { bean, token -> this.permissionRationaleSingle(bean, token) }
+                    .onResult(this::permissionResultSingle)
+                    .onRationale(this::permissionRationaleSingle)
                     .check()
         }
 
         val buttonLocation = findViewById<Button>(R.id.location_permission_button)
-        buttonLocation.setOnClickListener { _ ->
-            Mayi.withActivity(this)
+        buttonLocation.setOnClickListener {
+            MayI.withActivity(this)
                     .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
-                    .onResult { this.permissionResultSingle(it) }
-                    .onRationale { bean, token -> this.permissionRationaleSingle(bean, token) }
+                    .onResult(this::permissionResultSingle)
+                    .onRationale(this::permissionRationaleSingle)
                     .check()
         }
 
         val buttonAll = findViewById<Button>(R.id.all_permissions_button)
-        buttonAll.setOnClickListener { _ ->
-            Mayi.withActivity(this)
+        buttonAll.setOnClickListener {
+            MayI.withActivity(this)
                     .withPermissions(Manifest.permission.READ_CONTACTS, Manifest.permission.ACCESS_FINE_LOCATION)
-                    .onRationale { permissions, token -> this.permissionRationaleMulti(permissions, token) }
-                    .onResult { this.permissionResultMulti(it) }
-                    .onErrorListener { this.inCaseOfError(it) }
+                    .onRationale(this::permissionRationaleMulti)
+                    .onResult(this::permissionResultMulti)
+                    .onErrorListener(this::inCaseOfError)
                     .check()
         }
     }
@@ -54,12 +54,13 @@ class MainActivity : AppCompatActivity() {
     private fun permissionRationaleSingle(bean: PermissionBean, token: PermissionToken) {
         if (bean.simpleName.toLowerCase().contains("contacts")) {
             Toast.makeText(this, "Should show rationale for " + bean.simpleName + " permission", Toast.LENGTH_LONG).show()
+            Log.i("MainActivity", "Should show rationale for ${bean.simpleName}")
             token.skipPermissionRequest()
         } else {
             Toast.makeText(this, "Should show rationale for " + bean.simpleName + " permission", Toast.LENGTH_LONG).show()
+            Log.i("MainActivity", "Should show rationale for ${bean.simpleName}")
             token.continuePermissionRequest()
         }
-        Log.i("MainActivity", "Should show rationale for ${bean.simpleName}")
     }
 
     private fun permissionResultMulti(permissions: Array<PermissionBean>) {
